2f95788f5cfeb725b4e1525a21b9603750fcb427
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2023-09-09 11:37:53 -0300

    fixed bug in the ImportadProduct class and finished program

--- src/appliction/Program.java
+++ src/appliction/Program.java
@@ -1,5 +1,56 @@
 package appliction;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.ImportedProduct;
+import entities.Product;
+import entities.UsedProduct;
+
 public class Program {
+	public static void main(String[] args) throws ParseException {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdfIn = new SimpleDateFormat("dd/MM/yyyy");
+		List<Product> list = new ArrayList<Product>();
+		
+		System.out.print("Enter the number of products: ");
+		int N = sc.nextInt();
+		
+		for (int i = 1; i <= N; i++) {
+			System.out.printf("Product #%d data: %n", i);
+			System.out.print("Common, used or imported (c/u/i): ");
+			char ch = sc.next().charAt(0);
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.print("Price: ");
+			Double price = sc.nextDouble();
+			if (ch == 'i') {
+				System.out.print("Customs: ");
+				double customsFee = sc.nextDouble();
+				list.add(new ImportedProduct(name, price, customsFee));
+			} else if (ch == 'u') {
+				System.out.print("Manufature date (dd/MM/yyyy): ");
+				Date date = sdfIn.parse(sc.next());
+				list.add(new UsedProduct(name, price, date));
+			} else {
+				list.add(new Product(name, price));
+			}			
+		}
+		
+		System.out.println();
+		System.out.println("PRICE TAGS:");
+		for(Product product : list) {
+			System.out.println(product.priceTag());
+		}
+		
+		sc.close();
+	}
 
 }
--- src/entities/ImportedProduct.java
+++ src/entities/ImportedProduct.java
@@ -26,7 +26,7 @@ public class ImportedProduct extends Product{
 	
 	@Override
 	public String priceTag() {
-		return getName() + " $ " + String.format("%.2f", totalPrice()) + " (Customs fee: $ " + String.format("%.2f", getCustomsFee() + ")");
+		return getName() + " $ " + String.format("%.2f", totalPrice()) + " (Customs fee: $ " + String.format("%.2f", getCustomsFee()) + ")";
 	}
 	
 	
